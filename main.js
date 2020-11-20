6310e7db17616638fa97189b5a4a2decdd2ece2f
Comforter11 <71063972+Comforter11@users.noreply.github.com>
2020-11-20 06:12:04 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function greet(x,y){
+    console.log(x + y)
+}
+greet("Hello"+" "+"cossie");
+
+function multiple(x,y){
+    x = 3
+    y = 5
+    console.log(x*y)
+}
+multiple()
+
+function tableoffive(z){
+    z = 5
+    return console.log(z*5)
+}
+tableoffive()
